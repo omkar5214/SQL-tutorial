55a0822b4d246ba7d43575a2671443eee6ba3c0a
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-06-27 23:56:50 +0530

    Update window functions.sql

--- window functions.sql
+++ window functions.sql
@@ -22,3 +22,18 @@ from employee_demographics as dem join employee_salary as sal
  row_number() over(partition by gender order by salary desc) 
 from employee_demographics as dem join employee_salary as sal 
  on dem.employee_id=sal.employee_id;
+
+#rank -- can have duplicates but it will skip number number eg if number 4 is repeated once number 5 will be skipped directly to number 6
+select dem.first_name, gender,salary, 
+ row_number() over(partition by gender order by salary desc) as row_number,
+ rank() over(partition by gender order by salary desc) as rank
+from employee_demographics as dem join employee_salary as sal 
+ on dem.employee_id=sal.employee_id;
+
+#dense rank - repeatation is present but number wull not be skipped
+select dem.first_name, gender,salary, 
+ row_number() over(partition by gender order by salary desc) as row_number,
+ rank() over(partition by gender order by salary desc) as rank,
+ dense_rank() over(partition by gender order by salary desc) as rank
+from employee_demographics as dem join employee_salary as sal 
+ on dem.employee_id=sal.employee_id;
