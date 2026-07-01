4adba511c6da80dafa164e5e7d385782ec7dd829
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-07-01 22:54:15 +0530

    Update temporary table creation.sql

--- temporary table creation.sql
+++ temporary table creation.sql
@@ -13,3 +13,9 @@ values("alex","fernandes", "lord of the rings");
 
 select * from temp_table;
 
+#second way - this will only work in the section
+select* from employee_salary;
+create temporary table salary_over_50k 
+select * from employee_salary where salary>=50000;
+
+select * from salary_over_50k;
