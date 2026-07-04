da4bd440ba2917c2c8951b14c8b5c83af2bdc05b
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-07-04 23:31:44 +0530

    Add files via upload

---
+++ triggers and events.sql
@@ -0,0 +1,18 @@
+-- triggers and event
+# if we update in one table and forgot in other this trigger can help us to update in updating in other tables
+select * from employee_demographics;
+
+select * from employee_salary;
+
+delimiter $$
+create trigger employee_insert
+	after insert on employee_salary #we can also do before instead of after can be used for delete func
+    for each row
+begin
+	insert into employee_demographics (employee_id, first_name, last_name)
+    values (new.employee_id, new.first_name, new.last_name); #old - for delete or updated
+end $$
+delimiter ;
+# it does n't it own little section in the sechmas chart but can be in inside a tables click arrow button and u will find it
+
+    
