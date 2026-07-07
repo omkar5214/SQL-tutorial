58d478f4830d329bcc995bb4612b8ca6185f18a2
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-07-07 23:47:30 +0530

    Insert new employee salary record
    Added a new employee salary record for Jon Jin.

--- triggers and events.sql
+++ triggers and events.sql
@@ -15,3 +15,4 @@ end $$
 delimiter ;
 # it does n't it own little section in the sechmas chart but can be in inside a tables click arrow button and u will find it
 insert into employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
+values(15, "jon", "jin", "actor", "50000", null),
