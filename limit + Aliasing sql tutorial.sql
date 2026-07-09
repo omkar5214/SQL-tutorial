46ab0da918c569f63883f0933115fc955c1f6f43
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-07-09 23:27:42 +0530

    Add SQL examples for LIMIT and aliasing

--- limit + Aliasing sql tutorial.sql
+++ limit + Aliasing sql tutorial.sql
@@ -3,6 +3,7 @@
 select gender, avg(age) from employee_demographics group by gender having avg(age) > 40 LIMIT 3;
 select * from employee_demographics order by age desc LIMIT 3;
 select * from employee_demographics order by age desc LIMIT 3,1; # start at position 3 and go one row after
+ 
 -- alaising  changing name 
 select gender, avg(age) as avgage from employee_demographics group by gender having avgage>40;
  -- or we can skip as
