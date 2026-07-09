331aea8f4d0e42dad869badbade2970c7ce54dd7
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-07-09 23:27:27 +0530

    Update limit + Aliasing sql tutorial.sql

--- limit + Aliasing sql tutorial.sql
+++ limit + Aliasing sql tutorial.sql
@@ -1,4 +1,5 @@
 #...LIMIT AND ALIASING
+ #limit
 select gender, avg(age) from employee_demographics group by gender having avg(age) > 40 LIMIT 3;
 select * from employee_demographics order by age desc LIMIT 3;
 select * from employee_demographics order by age desc LIMIT 3,1; # start at position 3 and go one row after
