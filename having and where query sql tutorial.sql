6a9c2ff7a335c497ae3b90004c36b7ffd405b048
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-07-10 23:37:35 +0530

    Update having and where query sql tutorial.sql

--- having and where query sql tutorial.sql
+++ having and where query sql tutorial.sql
@@ -1,4 +1,5 @@
 # having and where together
+#having 
 select gender, avg(age) from employee_demographics group by gender having avg(age) > 40 ;
 
 select occupation, avg(salary) from employee_salary where occupation like %manager% group by occupation having avg(salary) > 4000 ;
