2135725f5f5ff0bc3b7deb1bcbcaa9d8ec637380
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-07-10 23:38:12 +0530

    Add SQL examples for HAVING and WHERE clauses

--- having and where query sql tutorial.sql
+++ having and where query sql tutorial.sql
@@ -2,5 +2,6 @@
 #having 
 select gender, avg(age) from employee_demographics group by gender having avg(age) > 40 ;
 
+#having clause using where clause 
 select occupation, avg(salary) from employee_salary where occupation like %manager% group by occupation having avg(salary) > 4000 ;
 
