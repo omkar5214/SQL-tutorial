d691d2b586698dc08198229cf8d900b5659832fb
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-07-11 23:56:03 +0530

    Fix comments and improve SQL select statement examples

--- select statement sql tutorial.sql
+++ select statement sql tutorial.sql
@@ -1,6 +1,6 @@
 # * - is used to fetch all the columns from table eg.
 select * from parks_and_recreation.employee_demographics;
-# we can also fetch single columns from the table eg
+# we can also fetch columns one by one from the table eg
 select first_name, last_name, birth_date, age, age+10 from parks_and_recreation.employee_demographics;
 # remember sql follows PEMDAS rule = - which is the order of calc of arithmatic or map in mysql, parenthese, exponential, multiplication, division , addition, subtraction. 
 select first_name, last_name, birth_date, age, (age+10)*100 from parks_and_recreation.employee_demographics;
