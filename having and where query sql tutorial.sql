d13abff1d399ff7d04cb14caf286bccfadba808b
omkar5214 <149251066+omkar5214@users.noreply.github.com>
2026-07-18 23:26:26 +0530

    Add SQL query for average age under 40

--- having and where query sql tutorial.sql
+++ having and where query sql tutorial.sql
@@ -1,6 +1,7 @@
 # having and where clauses together
 #having clause
 select gender, avg(age) from employee_demographics group by gender having avg(age) > 40 ;
+select gender, avg(age) from employee_demographics group by gender having avg(age) < 40 ;
 
 #having clause using where clause.
 select occupation, avg(salary) from employee_salary where occupation like %manager% group by occupation having avg(salary) > 4000 ;
